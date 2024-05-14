d264cbce5a4d03b493194c028ce8161cccbbb5a5
Lance Kalbach <lancekalbach@gmail.com>
2024-05-14 16:27:20 -0600

    new repo

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/navbar.css'
+
+export default function navbar() {
+    return (
+        <div className="navbar-div">
+            testing the navbar component
+        </div>
+    )
+}
