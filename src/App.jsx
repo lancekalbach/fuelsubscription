a8a85b040d40008894ded07136a1dfa4fd81492d
Lance Kalbach <lancekalbach@gmail.com>
2024-05-14 16:45:23 -0600

    moving things to fix things

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles/App.css'
+import Navbar from './components/navbar'
+import About from './components/about'
+
+export default function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <About/>
+    </>
+  )
+}
